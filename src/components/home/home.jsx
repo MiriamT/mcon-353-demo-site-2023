a532e3d350302420c1e0defce52896357de022a0
MiriamT <MiriamT@users.noreply.github.com>
2023-02-09 19:51:38 -0500

    moving home page to new component

---
+++ src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import { Button, Card } from "@mui/material";
+import logo from "../../logo.svg";
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <Button className="button-class" variant="contained">
+        hello
+      </Button>
+    </div>
+  );
+};
---
+++ src/components/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Todo = () => {
+  return (
+    <div>
+      <h1>Todos</h1>
+      <p>cook for Shabbos</p>
+      <p>finish homework</p>
+      <p>hang out with friends</p>
+    </div>
+  );
+};
