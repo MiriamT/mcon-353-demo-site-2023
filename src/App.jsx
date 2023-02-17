5ec7861504252a5f3a3ec0a48390fb8ad9ca0a6b
MiriamT <MiriamT@users.noreply.github.com>
2023-02-16 19:36:47 -0500

    header with routes for home and todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Home } from "./components/home/home";
 import { Todo } from "./components/todo/todo";
+import { Header } from "./components/header/header";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  // return <Home />;
-  return <Todo />;
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        {/* http://localhost:3000/#/ */}
+        <Route path="/" element={<Home />} />
+        {/* http://localhost:3000/#/todo */}
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
